1c523701726e6d6eec0d8c3d36445aec4585d426
AnaduCG <godzionwin@gmail.com>
2023-12-25 20:33:56 +0100

    template setup for index and create-task

--- todo_project/todo_app/models.py
+++ todo_project/todo_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.timezone import now
+from uuid import  uuid4
+
 
 # Create your models here.
+class Todo(models.Model):
+    """Model definition for Todo."""
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    title = models.CharField(max_length=200, null=True, blank=True, unique=True)
+    body = models.TextField(null=True, blank=True)
+    created = models.DateTimeField(editable=False, default=now)
+    updated = models.DateTimeField(auto_now=now)
+    done = models.BooleanField(default=False)
+
+    class Meta:
+        """Meta definition for Todo."""
+
+        verbose_name = "Todo"
+        verbose_name_plural = "Todos"
+
+    def __str__(self):
+        """Unicode representation of Todo."""
+        return str(self.title)
--- todo_project/todo_app/views.py
+++ todo_project/todo_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    """index view function"""
+    context = {}
+    return render(request, "index.html", context)
+
+
+def create(request):
+    """index view function"""
+    context = {}
+    return render(request, "create-task.html", context)
